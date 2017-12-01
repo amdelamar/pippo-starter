d27cabab10ecd8e05afe27cebb1eba8690b2b1e3
amdelamar <amdelamar@protonmail.com>
2017-12-01 15:13:26 -0500

    Fix template engine

--- src/main/java/hello/Pippo.java
+++ src/main/java/hello/Pippo.java
@@ -1,10 +1,54 @@
 package hello;
 
-public class Pippo {
+import ro.pippo.core.Application;
+
+public class Pippo extends Application {
 
     public static void main(String[] args) {
-        ro.pippo.core.Pippo pippo = new ro.pippo.core.Pippo(new BasicApplication());
+        ro.pippo.core.Pippo pippo = new ro.pippo.core.Pippo(new Pippo());
+
+        // change default port
         pippo.getServer().getSettings().port(8080);
+
+        // add routes for static content
+        pippo.addPublicResourceRoute();
+        pippo.addWebjarsResourceRoute();
+
+        // start server
         pippo.start();
     }
+
+    @Override
+    protected void onInit() {
+
+        // send a template as response
+        GET("/", routeContext -> {
+            routeContext.setLocal("greeting", "Hello World!");
+            routeContext.render("hello.ftl");
+        });
+
+        // send a json as response
+        GET("/json", routeContext -> {
+            Contact contact = createContact();
+            routeContext.json()
+                    .send(contact);
+        });
+
+        // send xml as response
+        GET("/xml", routeContext -> {
+            Contact contact = createContact();
+            routeContext.xml()
+                    .send(contact);
+        });
+    }
+
+    private Contact createContact() {
+        Contact contact = new Contact();
+        contact.setId(12345);
+        contact.setName("John");
+        contact.setPhone("0733434435");
+        contact.setAddress("Sunflower Street, No. 6");
+        return contact;
+    }
+
 }
---
+++ src/main/resources/templates/hello.ftl
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta charset="utf-8"/>
+    <meta http-equiv="x-ua-compatible" content="ie=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <link rel="shortcut icon" href="public/favicon.ico"/>
+    <link rel="icon" type="image/x-icon" href="public/favicon.ico"/>
+    <title>Pippo App</title>
+</head>
+<body style="text-align:center;background:#fafafa;font-family:sans-serif;">
+
+	<img src="public/logo.png" alt="Pippo" style="max-width:100%;max-height:100%;" />
+	<h1>${greeting}</h1>
+</body>
+</html>
